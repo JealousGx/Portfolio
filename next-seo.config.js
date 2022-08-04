cd08ecc37d175eeb1a6b7dcd193ae9b87923672c
JealousGx <khiljimateenn@gmail.com>
2022-08-04 21:42:47 +0500

    Major changes

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,33 +1,33 @@
 const defaultSEOConfig = {
-  title: "Anish De | Fullstack Developer",
+  title: "Matt | Front End Web Developer",
   description:
-    "10th grader • Fullstack Web Developer • Web3 Enthusiast • Blogger • Tech Enthusiast",
-  cannonical: "https://anishde.dev/",
+    "Final Year Engineering Student • Front End Web Developer • Gamer • Tech Enthusiast",
+  cannonical: "https://jealousdev.tech/",
   openGraph: {
     type: "website",
-    url: "https://anishde.dev/",
-    site_name: "Anish De",
-    title: "Anish De | Fullstack Developer",
+    url: "https://jealousdev.tech/",
+    site_name: "Matt",
+    title: "Matt | Front End Web Developer",
     description:
-      "10th grader • Fullstack Web Developer • Web3 Enthusiast • Blogger • Tech Enthusiast",
-    images: [
-      {
-        url: "https://res.cloudinary.com/anishde12020/image/upload/v1654360780/Blogfolio/og.png",
-        width: 1200,
-        height: 630,
-        alt: "Anish De's Portfolio Site's Opengraph Image",
-      },
-    ],
+      "Final Year Engineering Student • Front End Web Developer • Gamer • Tech Enthusiast",
+    // images: [
+    //   {
+    //     url: "https://res.cloudinary.com/anishde12020/image/upload/v1654360780/Blogfolio/og.png",
+    //     width: 1200,
+    //     height: 630,
+    //     alt: "Anish De's Portfolio Site's Opengraph Image",
+    //   },
+    // ],
     profile: {
-      firstName: "Anish",
-      lastName: "De",
-      username: "AnishDe12020",
+      firstName: "Abdul Mateen",
+      lastName: "Khilji",
+      username: "jealousgx",
       gender: "male",
     },
   },
   twitter: {
-    handle: "@AnishDe12020",
-    site: "@AnishDe12020",
+    handle: "@JealousGx",
+    site: "@JealousGx",
     cardType: "summary_large_image",
   },
 };
