363a5a40bd0e4dfc7cced0a55b67a9ee5ab2e96c
JealousGx <khiljimateenn@gmail.com>
2022-08-05 22:38:12 +0500

    minor ui changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -72,7 +72,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
           <CustomToaster />
           {isEnabled && circlesAllowed && (
-            <div className="fixed top-20 left-32 -z-10">
+            <div className="fixed top-20 md:left-32 sm:left-0 sm:right-0 sm:top-10 -z-10">
               <div className="absolute left-8 top-16 -z-10 overflow-visible opacity-20">
                 <div className="circle-1 -z-10 h-[400px] w-[700px] rounded-full bg-red-700 mix-blend-multiply blur-[128px]" />
               </div>
