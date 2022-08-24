1ebc593b892287d25e69997d16f192e165a2cd4c
Matt <59867217+JealousGx@users.noreply.github.com>
2022-08-24 07:57:37 +0000

    removes unnecessary imports / text

--- components/Common/Footer/Footer.tsx
+++ components/Common/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import Link from "@/components/Shared/Link";
-import Socials from "@/components/Shared/Socials";
 
 const ITEM_CLASSNAMES =
   "cursor-pointer capitalize text-gray-600 transition hover:text-gray-700 dark:text-gray-500 dark:hover:text-gray-600";
@@ -7,12 +6,7 @@ const ITEM_CLASSNAMES =
 const Footer = (): JSX.Element => {
   return (
     <footer className="mx-auto mb-16 flex max-w-3xl flex-col items-center space-y-2  px-8 text-center">
-      {/* <Socials small className="mb-8" />
-      <p>
-        Managed by{" "}
-        <Link href="https://github.com/JealousGx">JealousGx | Matt</Link>
-      </p> */}
-      <hr className="border-1 mb-8 w-full border-gray-200 dark:border-gray-800" />
+      <hr className="border-1 mb-8 w-full border-gray-300 dark:border-gray-800" />
       <div className="flex w-full flex-col items-center justify-between sm:flex-row">
         <div className="flex flex-col items-start gap-4">
           <Link href="/" noGradientUnderline noHighlight>
