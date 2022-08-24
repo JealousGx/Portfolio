a518a20fe8689c4eeada05ff42c01623b2027223
Matt <59867217+JealousGx@users.noreply.github.com>
2022-08-24 07:13:53 +0000

    fixes snippets and adds toggle theme tooltip

--- components/Common/Header/ThemeToggler.tsx
+++ components/Common/Header/ThemeToggler.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 
+import Tooltip from "@/components/Shared/Tooltip";
 import IconFactory from "@/components/Shared/Icons/IconFactory";
 
 const CLASSNAMES =
-  "rounded-lg transition linear duration-200 p-2 hover:ring-2 hover:ring-gray-300";
+  "rounded-lg transition cursor-pointer linear duration-200 p-2 hover:ring-2 hover:ring-gray-300";
 
 const ThemeToggler = (): JSX.Element => {
   const [mounted, setMounted] = useState(false);
@@ -21,21 +22,25 @@ const ThemeToggler = (): JSX.Element => {
 
   if (currentTheme === "dark") {
     return (
-      <div
-        className={`bg-gray-600 bg-opacity-40 ${CLASSNAMES}`}
-        onClick={() => setTheme("light")}
-      >
-        <IconFactory name="light" />
-      </div>
+      <Tooltip content="Toggle theme">
+        <div
+          className={`bg-gray-600 bg-opacity-40 ${CLASSNAMES}`}
+          onClick={() => setTheme("light")}
+        >
+          <IconFactory name="light" />
+        </div>
+      </Tooltip>
     );
   } else {
     return (
-      <div
-        className={`bg-gray-200 ${CLASSNAMES}`}
-        onClick={() => setTheme("dark")}
-      >
-        <IconFactory name="dark" />
-      </div>
+      <Tooltip content="Toggle theme">
+        <div
+          className={`bg-gray-200 ${CLASSNAMES}`}
+          onClick={() => setTheme("dark")}
+        >
+          <IconFactory name="dark" />
+        </div>
+      </Tooltip>
     );
   }
 };
