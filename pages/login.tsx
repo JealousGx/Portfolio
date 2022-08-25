327275dd796a8f51ecc661a26c1756659481d21f
Matt <59867217+JealousGx@users.noreply.github.com>
2022-08-25 12:01:37 +0000

    build: login btn fixes

--- pages/login.tsx
+++ pages/login.tsx
@@ -89,7 +89,7 @@ const Revalidate: NextPage = () => {
               />
               <button
                 type="submit"
-                className="w-full rounded-lg bg-gray-200 px-4 py-2 text-black transition duration-200 hover:ring-2 hover:ring-gray-300 dark:bg-gray-600 dark:bg-opacity-40 dark:text-white"
+                className="rounded-lg border-[1px] border-gray-300  bg-gray-50 px-4 py-2 transition hover:border-gray-200 dark:border-tertiary dark:bg-secondary dark:hover:border-accent"
               >
                 Login
               </button>
