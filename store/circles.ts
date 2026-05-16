65990ce07ce74087e82868606c2b4491ab04850e
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-05-16 16:43:48 +0500

    init

--- store/circles.ts
+++
@@ -1,9 +0,0 @@
-import type CirclesState from "types/circles";
-import { create } from "zustand";
-
-const useCircles = create<CirclesState>(set => ({
-  isEnabled: false,
-  toggleCircles: () => set(state => ({ isEnabled: !state.isEnabled })),
-}));
-
-export default useCircles;
