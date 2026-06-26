c8f3f725603a77845c4cd55f8a59842ad0b10160
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-06-26 12:01:08 +0500

    feat: update GuestbookClient layout for improved styling and structure

--- app/guestbook/client.tsx
+++ app/guestbook/client.tsx
@@ -2,33 +2,30 @@
 
 import Giscus from "@giscus/react";
 import { useTheme } from "next-themes";
-import React from "react";
 
 export default function GuestbookClient() {
     const { resolvedTheme } = useTheme();
     return (
-        <React.Fragment>
-            <h1 className="text-3xl font-bold">GuestBook</h1>
-            <p className="mt-4 text-gray-600 dark:text-gray-300">
-                Leave a message 😃
-            </p>
-            <div className="mt-16">
-                <Giscus
-                    id="comments"
-                    repo="JealousGx/Portfolio"
-                    repoId="MDEwOlJlcG9zaXRvcnkyOTE5MTkzOTc="
-                    category="Comments"
-                    categoryId="DIC_kwDOEWZWJc4CQmb0"
-                    mapping="specific"
-                    term="GuestBook"
-                    reactionsEnabled="1"
-                    emitMetadata="0"
-                    inputPosition="top"
-                    theme={resolvedTheme === "dark" ? "dark" : "light"}
-                    lang="en"
-                    loading="lazy"
-                />
+        <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-semibold tracking-tight">Guestbook</h1>
+                <p className="text-sm text-muted-foreground">Leave a message 😃</p>
             </div>
-        </React.Fragment>
+            <Giscus
+                id="comments"
+                repo="JealousGx/Portfolio"
+                repoId="MDEwOlJlcG9zaXRvcnkyOTE5MTkzOTc="
+                category="Comments"
+                categoryId="DIC_kwDOEWZWJc4CQmb0"
+                mapping="specific"
+                term="GuestBook"
+                reactionsEnabled="1"
+                emitMetadata="0"
+                inputPosition="top"
+                theme={resolvedTheme === "dark" ? "dark" : "light"}
+                lang="en"
+                loading="lazy"
+            />
+        </div>
     );
 }
